55911f9ac8936dbc6e74dbe4c24407bc5529f3d7
Vinicius <vinicius.silva.grilo@gmail.com>
2020-07-18 17:30:59 -0300

    Screening City Filter

---
+++ app/Http/Controllers/ScreeningController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sample;
+use App\LabSample;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class ScreeningController extends Controller
+{
+
+    public function show($residential_city){
+
+        return Sample::where('residential_city',$residential_city)
+        ->where('confirmation', 0)
+        ->get();
+
+        return $sampleFilter;
+
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('screening/{residential_city}','ScreeningController@show');
 
 Route::resource('users', 'UserController');
 
